04bc98bbe5d2f95576fbcfcde38e13546c527ae2
Adrian Hesketh <adrianhesketh@hushmail.com>
2020-04-22 11:19:04 +0100

    First commit.

---
+++ src/features/counter/Counter.js
@@ -0,0 +1,114 @@
+import React, { useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+import {
+  previous,
+  answer,
+  selectIndex,
+  selectCount,
+  selectQuestion,
+  selectResult,
+  selectEnd,
+} from "./counterSlice";
+import { VictoryBar, VictoryChart } from "victory";
+import styles from "./Counter.module.css";
+
+export function Counter() {
+  const index = useSelector(selectIndex);
+  const count = useSelector(selectCount);
+  const question = useSelector(selectQuestion);
+  const scores = useSelector(selectResult);
+  const isEnd = useSelector(selectEnd);
+  const dispatch = useDispatch();
+
+  if (isEnd) {
+    return (
+      <>
+        <div className={styles.row}>
+          <h1>Your results...</h1>
+        </div>
+        <div className={styles.row} style={{ width: 400 }}>
+          <VictoryChart domainPadding={{ x: 20 }}>
+            <VictoryBar
+              data={scoresToData(scores)}
+              x="platform"
+              y="score"
+              domain={{ y: [-10, 25] }}
+            />
+          </VictoryChart>
+        </div>
+      </>
+    );
+  }
+
+  return (
+    <div>
+      <div className={styles.row}>
+        <h1>
+          Question {index + 1} of {count}
+        </h1>
+      </div>
+      <div className={styles.row}>{question.q}</div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          style={{ marginRight: 20 }}
+          aria-label="Yes"
+          onClick={() => dispatch(answer(false))}
+        >
+          No
+        </button>
+        <button
+          className={styles.button}
+          style={{ marginLeft: 20 }}
+          aria-label="Yes"
+          onClick={() => dispatch(answer(true))}
+        >
+          Yes
+        </button>
+      </div>
+      <div className={styles.row}>
+        <button
+          className={styles.button}
+          style={{ marginRight: 20 }}
+          aria-label="Back"
+          onClick={() => dispatch(previous())}
+        >
+          Back
+        </button>
+      </div>
+      <div className={styles.row}>
+        <h3>EC2</h3>
+      </div>
+      <div className={styles.row}>
+        <p>{question.ec2.rationale}</p>
+      </div>
+      <div className={styles.row}>
+        <h3>Containers</h3>
+      </div>
+      <div className={styles.row}>
+        <p>{question.containers.rationale}</p>
+      </div>
+      <div className={styles.row}>
+        <h3>Serverless</h3>
+      </div>
+      <div className={styles.row}>
+        <p>{question.lambda.rationale}</p>
+      </div>
+    </div>
+  );
+}
+
+const scoresToData = (scores) => [
+  {
+    platform: "ec2",
+    score: scores.ec2,
+  },
+  {
+    platform: "containers",
+    score: scores.containers,
+  },
+  {
+    platform: "lambda",
+    score: scores.lambda,
+  },
+];
---
+++ src/features/counter/counterSlice.js
@@ -0,0 +1,101 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+export const counterSlice = createSlice({
+  name: "counter",
+  initialState: {
+    index: 0,
+    questions: [
+      {
+        id: "q1",
+        q: `Do you want to reduce the amount of time that your team spend planning and designing infrastructure?`,
+        ec2: {
+          true: -1,
+          false: 0,
+          rationale: `VM infrastructure typically requires large amounts of up-front planning to configure storage and plan physical infrastructure.`,
+        },
+        containers: {
+          true: 1,
+          false: 0,
+          rationale: `Containers can reduce the amount of up-front planning, however, the use of complex container orchestration platforms such as Kubernetes can mean that the amount of reduction is reduced overall.`,
+        },
+        lambda: {
+          true: 2,
+          false: 0,
+          rationale: `Serverless architectures are typically focussed on service interactions, resulting in lower amounts of planning and design efforts.`,
+        },
+      },
+      {
+        id: "q2",
+        q:
+          "Do you want to reduce the amount of time that your team spend patching servers?",
+        ec2: {
+          true: -1,
+          false: 0,
+          rationale: `VM infrastructure offers minimal benefits, offering a platform to run operating systems on. Patching and maintenance is carried out with 3rd party desired state configuration tools such as Chef, Puppet, Ansible or Powershell DSC.`,
+        },
+        containers: {
+          true: 1,
+          false: 0,
+          rationale: `Use of a Serverless container platform such as AWS Fargate can reduce the amount of effort expended on server patching, however, the containers themselves may contain operating system dependencies, and require regular updates.`,
+        },
+        lambda: {
+          true: 3,
+          false: 0,
+          rationale: `Serverless applications use a fully managed operating system and container platform requiring no user management. The security of the platform is managed by AWS.`,
+        },
+      },
+    ],
+  },
+  reducers: {
+    next: (state) => {
+      if(state.index == state.questions.length-1) {
+        return;
+      }
+      state.index += 1;
+    },
+    previous: (state) => {
+      if(state.index == 0) {
+        return;
+      }
+      state.index -= 1;
+    },
+    answer: (state, payload) => {
+      if (!state.answers || state.answers.length != state.questions.length) {
+        state.answers = new Array(state.questions.length);
+      }
+      state.answers[state.index] = payload.payload;
+      if(state.index == state.questions.length-1) {
+        state.end = true;
+        return;
+      }
+      state.index += 1;
+    },
+  },
+});
+
+export const { next, previous, answer } = counterSlice.actions;
+
+export const selectIndex = state => state.counter.index;
+export const selectCount = state => state.counter.questions.length;
+export const selectQuestion = state => state.counter.questions[state.counter.index];
+export const selectResult = state => {
+  const result = {
+    ec2: 0,
+    containers: 0,
+    lambda: 0,
+  };
+  if(!state.counter.answers) {
+    return result;
+  }
+  for(let i = 0; i < state.counter.answers.length; i ++) {
+    const answer = state.counter.answers[i]
+    const q = state.counter.questions[i];
+    result.ec2 += q.ec2[answer] || 0;
+    result.containers += q.containers[answer] || 0;
+    result.lambda += q.containers[answer] || 0;
+  }
+  return result;
+};
+export const selectEnd = state => state.counter.end || false;
+
+export default counterSlice.reducer;
