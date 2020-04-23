6e3f173ada1fc85654b66ca1656c735ee4bec03f
Adrian Hesketh <adrianhesketh@hushmail.com>
2020-04-23 17:16:52 +0100

    Updates

---
+++ precache-manifest.86da43c86aa9cc7931399a1c79a6a732.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ba96b41ea59845d68819a3cc03032ed8",
+    "url": "/compute/index.html"
+  },
+  {
+    "revision": "8e4ab871cbbffa9c642c",
+    "url": "/compute/static/css/main.17590498.chunk.css"
+  },
+  {
+    "revision": "017ff8b4b58c499cb3c7",
+    "url": "/compute/static/js/2.984ef1d7.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/compute/static/js/2.984ef1d7.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "8e4ab871cbbffa9c642c",
+    "url": "/compute/static/js/main.73b2f07f.chunk.js"
+  },
+  {
+    "revision": "0efd12f170ab49bccdf4",
+    "url": "/compute/static/js/runtime-main.025f2615.js"
+  }
+]);
