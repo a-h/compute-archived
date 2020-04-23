e96cfdad165400d619266ad74496e7a53dcb40f1
Ben <ben@benchilds.uk>
2020-04-23 11:33:44 +0100

    Reworked SASS approach. Questions page done.

--- src/features/counter/Counter.js
+++ src/features/counter/Counter.js
@@ -45,6 +45,15 @@ export function Counter() {
   return (
     <div>
       <header className="app-header">
+      <div className={styles.row}>
+          <button
+            className={styles['button-back']}
+            aria-label="Back"
+            onClick={() => dispatch(previous())}
+          >
+            Back
+          </button>
+        </div>
         <div className={styles.row}>
           <h1 className={styles.progress}>
             Question <span className={styles['q-num']}>{index + 1}</span> of <span className={styles['q-total']}>{count}</span>
@@ -57,45 +66,31 @@ export function Counter() {
         </div>
         <div className={styles.row}>
           <button
-            className={styles.button + ' ' + styles.no}
-            style={{ marginRight: 20 }}
+            className={styles.button}
             aria-label="No"
             onClick={() => dispatch(answer(false))}
             >
             No
           </button>
           <button
-            className={styles.button + ' ' + styles.yes}
-            style={{ marginLeft: 20 }}
+            className={styles.button}
             aria-label="Yes"
             onClick={() => dispatch(answer(true))}
           >
             Yes
           </button>
         </div>
-        <div className={styles.row}>
-          <button
-            className={styles.button + ' ' + styles.back}
-            style={{ marginRight: 20 }}
-            aria-label="Back"
-            onClick={() => dispatch(previous())}
-          >
-            Back
-          </button>
-        </div>
       </article>
       <aside className="app-aside">
         <div className={styles.row}>
-          <h3 className={styles.platform}>EC2</h3>
-          <p className={styles.rationale}>{question.ec2.rationale}</p>
-        </div>
-        <div className={styles.row}>
-          <h3 className={styles.platform}>Containers</h3>
-          <p className={styles.rationale}>{question.containers.rationale}</p>
-        </div>
-        <div className={styles.row}>
-          <h3 className={styles.platform}>Serverless</h3>
-          <p className={styles.rationale}>{question.lambda.rationale}</p>
+          <dl className={styles.rationale}>
+            <dt><h3>EC2</h3></dt>
+            <dd>{question.ec2.rationale}</dd>
+            <dt><h3>Containers</h3></dt>
+            <dd>{question.containers.rationale}</dd>
+            <dt><h3>Serverless</h3></dt>
+            <dd>{question.lambda.rationale}</dd>
+          </dl>
         </div>
       </aside>
     </div>
